22ac99b24d7c42c6fdcc28ff6862af2d818eb657
nghiatd1184 <nghiatd.dhtm@gmail.com>
2024-03-13 16:49:11 +0700

    [feat] nghiatran

--- app/src/main/java/edu/t3h/note/HomeScreen.kt
+++ app/src/main/java/edu/t3h/note/HomeScreen.kt
@@ -1,7 +1,9 @@
 package edu.t3h.note
 
+import android.content.Intent
 import android.icu.text.SimpleDateFormat
 import android.os.Bundle
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import java.util.Date
@@ -10,6 +12,7 @@ import java.util.Locale
 
 class HomeScreen:AppCompatActivity() {
     private lateinit var day : TextView
+    private lateinit var createNoteBtn : LinearLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +22,12 @@ class HomeScreen:AppCompatActivity() {
         val formatter = SimpleDateFormat("dd MMMM, YYYY", Locale.ENGLISH)
         val date = Date()
         val current = formatter.format(date)
-        day.text = "$current"
+        day.text = current
+
+        createNoteBtn = findViewById(R.id.menuCreateNote)
+        createNoteBtn.setOnClickListener{
+            startActivity(Intent(this, CreateNote::class.java))
+            finish()
+        }
     }
 }
---
+++ app/src/main/java/edu/t3h/note/Manager.kt
@@ -0,0 +1,7 @@
+package edu.t3h.note
+
+object Manager {
+    var step = 0
+    var title = ""
+    var body = ""
+}
