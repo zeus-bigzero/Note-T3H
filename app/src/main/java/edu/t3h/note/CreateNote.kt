4ffebd4292781d2de7ade5f577f5e2da8e420ae6
NgVanQuy2003 <nvquy12346789@gmail.com>
2024-03-14 00:42:07 +0700

    [feat] Tutorial screen

---
+++ app/src/main/java/edu/t3h/note/CreateNote.kt
@@ -0,0 +1,11 @@
+package edu.t3h.note
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CreateNote : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_note)
+    }
+}
---
+++ app/src/main/java/edu/t3h/note/Home_activity.kt
@@ -0,0 +1,11 @@
+package edu.t3h.note
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Home_activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
